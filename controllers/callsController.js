333039f2765b5b21c02d7037d8ca91123d2d7857
dNem88 <82020442+dNem88@users.noreply.github.com>
2022-04-01 11:26:09 +0300

    calls page

---
+++ controllers/callsController.js
@@ -0,0 +1,25 @@
+const callsController = require('express').Router()
+const ObjectId = require('mongodb').ObjectId
+
+callsController.get('/', async(req,res,next) => {
+    try{
+        let calls = await req.app.locals.client.db('realestate').collection('calls').find().toArray();
+        console.log(calls)
+        res.status(200).json(calls)
+    }catch(err) {
+        res.status(400).json(err)
+    }
+   
+})
+callsController.post('/', async(req,res,next) => {
+    const data = req.body
+     try{
+        let response = await req.app.locals.client.db('realestate').collection('calls').insertOne({...data, date: Date()})
+        res.status(201).json(response)
+        console.log(response)
+    } catch(e) {
+        res.status(400).json('Failed to insert data!')
+    }
+})
+
+module.exports = callsController
--- routes.js
+++ routes.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const authController = require('./controllers/authController');
 const offersController = require('./controllers/offersController')
 const dealsController = require('./controllers/dealsController')
+const callsController = require('./controllers/callsController')
 const isAuthorized = require('./middlewares/isAuthorized')
 
 
 router.use('/users', authController);
 router.use('/offers', offersController);
-router.use('/deals', dealsController)
+router.use('/deals', dealsController);
+router.use('/calls', callsController)
 
 
 
