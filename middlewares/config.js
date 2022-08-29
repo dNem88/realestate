3d9fffd26286f8bc67446204d7eb8b4e2c26d257
dNem88 <82020442+dNem88@users.noreply.github.com>
2022-08-29 14:45:12 +0300

    cors all

--- middlewares/config.js
+++ middlewares/config.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
     app.use(express.json());
     app.use(cors({
         methods: 'GET,POST,PUT,DELETE, OPTIONS',
-        origin: false,
+        origin: '*',
         preflightContinue: true,
         allowedHeaders: "Content-Type, Authorization, X-Requested-With",
         credentials: true,
