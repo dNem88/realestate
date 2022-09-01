1ac301e04d6675c2d09473f770baff81a33e9724
dNem88 <82020442+dNem88@users.noreply.github.com>
2022-09-01 16:22:52 +0300

    * cors

--- middlewares/config.js
+++ middlewares/config.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
     app.use(express.json());
     app.use(cors({
         methods: 'GET,POST,PUT,DELETE',
-        origin: "https://dnem88.github.io/",
+        origin: "*",
         preflightContinue: true,
         allowedHeaders: "Content-Type, Authorization, X-Requested-With",
         credentials: true,
