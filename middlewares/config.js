88de6145dd1b4c7ad5e38768d9e1ab38a89dc533
dNem88 <82020442+dNem88@users.noreply.github.com>
2022-09-01 13:07:46 +0300

    cors

--- middlewares/config.js
+++ middlewares/config.js
@@ -10,11 +10,11 @@ module.exports = (app) => {
     }));
     app.use(express.json());
     app.use(cors({
-        methods: 'GET,POST,PUT,DELETE,HEAD',
+        methods: 'GET,POST,PUT,DELETE',
         origin: ['https://dnem88.github.io/',
             'http://localhost:3000'
         ],
-        preflightContinue: false,
+        preflightContinue: true,
         allowedHeaders: "Content-Type, Authorization, X-Requested-With",
         credentials: true,
         optionsSuccessStatus: 200
@@ -22,4 +22,5 @@ module.exports = (app) => {
     app.use(cookieParser(process.env.SECRET_KEY))
     app.set('view engine', 'ejs');
     app.use('/static', express.static(`${root}/static`));
+    console.log('here')
 };
