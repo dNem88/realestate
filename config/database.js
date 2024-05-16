24a73245a5b0d9f0c5b916382018320669c6cf94
dNem88 <82020442+dNem88@users.noreply.github.com>
2024-05-16 16:20:10 +0300

    Error handling in db onfig

--- config/database.js
+++ config/database.js
@@ -2,26 +2,6 @@ const {
     MongoClient
 } = require('mongodb');
 
-// const client = new MongoClient("mongodb+srv://dNem88:daniel123$$@dn88.vllop.mongodb.net/realestate?retryWrites=true&w=majority", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-
-
-// module.exports = (app) => {
-//     client.connect((err, db) => {
-//         if (err) {
-//             console.log('Failed to connect to database!');
-//         }
-//         if (db) {
-//             console.log('Connected to MongoDB Atlas Realestate');
-//             app.locals.client = db;
-//             app.listen(process.env.PORT || 4000, console.log(`Server listens on port ${process.env.PORT} and you can make your requests`));
-//             /*Keep a reference to the database in app.locals*/
-//         }
-//     });
-// };
-
 
 const uri = process.env.MONGODB_URI;
 const options = {
@@ -32,9 +12,9 @@ console.log(process.env.MONGODB_URI)
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
-    throw new Error('Add Mongo Uri to env.local')
-}
+// if (!process.env.MONGODB_URI) {
+//     throw new Error('Add Mongo Uri to .env')
+// }
 
 if (process.env.NODE_ENV === "development") {
     if (!global._mongoClientPromise) {
